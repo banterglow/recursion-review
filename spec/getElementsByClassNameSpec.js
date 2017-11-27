8415b63223345579dd4aedf67241b5c1d31e87f3
wongal5 <alberthwong2@gmail.com>
2017-11-27 13:25:52 -0800

    Completed getElementsByClassName

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -20,7 +20,7 @@ describe('getElementsByClassName', function() {
       var expectedNodeList = document.getElementsByClassName('targetClassName');
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
-      expect(equality).to.equal(FILL_ME_IN);
+      expect(equality).to.equal(true);
 
       $rootElement.remove();
     });
@@ -28,3 +28,5 @@ describe('getElementsByClassName', function() {
   });
 
 });
+
+//[body, span, span, span, span, div]
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,6 +5,40 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
-) {
-  // your code here
+) {  
+
+  var resultArray = [];
+
+  var checkElement = function(element) {
+    if(element.classList !== undefined && element.classList.contains(className)) {
+      resultArray.push(element);      
+    }
+    if(element.childNodes !== undefined) {
+      var childrenArray = element.childNodes;
+      for (let j = 0; j < childrenArray.length; j++) {
+        checkElement(childrenArray[j]);    
+      }
+    }
+  };
+  
+  checkElement(document.body);
+
+  // for (let i = 0; i < elementArray.length; i++) {
+  //   if (elementArray[i].classList.contains(className)) {
+  //     resultArray.push(elementArray[i]);
+  //   }
+
+  //   if (elementArray[i].children) {
+  //     var childrenArray = elementArray[i].children;
+  //     for (let j = 0; j < childrenArray.length; j++) {
+  //       resultArray = resultArray.concat(childrenArray[j].getElementsByClassName(className));      
+  //     }
+  //   }
+  // }
+
+  return resultArray;
 };
+
+// string
+// use value to check if matches given string
+// add to central array
