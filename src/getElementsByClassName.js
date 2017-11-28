0430b2ac22cbc59f3ff1849f36a24eba529f0807
wongal5 <alberthwong2@gmail.com>
2017-11-27 16:34:37 -0800

    Clarifying comments. Completed stringify and getElements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -12,12 +12,12 @@ var getElementsByClassName = function(className) {
 //helper function to check each element individually, then check for child nodes
   var checkElement = function(element) {
     //check for class in element's class list
-    if(element.classList !== undefined && element.classList.contains(className)) {
+    if (element.classList !== undefined && element.classList.contains(className)) {
       resultArray.push(element);      
     }
     
     // check for child nodes and iterate on each if found
-    if(element.childNodes !== undefined) {
+    if (element.childNodes !== undefined) {
       var childrenArray = element.childNodes;
       for (let j = 0; j < childrenArray.length; j++) {
         checkElement(childrenArray[j]);    
